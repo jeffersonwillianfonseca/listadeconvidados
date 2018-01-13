b62b41aa2de14dc3c8c6380eeb7e2fd55772011e
JeffersonWillian <jefferson.has.fonseca@gmail.com>
2018-01-13 10:43:57 -0200

    Adiciona o projeto inicial

---
+++ src/main/java/br/com/utilitarios/lista/ListaConvidados.java
@@ -0,0 +1,11 @@
+package br.com.utilitarios.lista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaConvidados {
+    public static void main(String[] args) {
+        SpringApplication.run(ListaConvidados.class, args);
+    }
+}
